a962f1f60b372be1b92a3518ded3128e1a725f70
Jens Mollerhoj <mollerhoj3@gmail.com>
2011-12-01 13:48:56 +0100

    deleted sql dump

--- DigitalVoterList/DigitalVoterList/Election/DAOMysql.cs
+++ DigitalVoterList/DigitalVoterList/Election/DAOMysql.cs
@@ -14,7 +14,92 @@ namespace DigitalVoterList.Election
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
-    public class DAOMysql
+    public class DAOMysql : IDataAccessObject
     {
+
+        public Person LoadPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User LoadUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User LoadUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VoterCard LoadVoterCard(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VoterCard LoadVoterCard(string idKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> Find(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> Find(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<VoterCard> Find(VoterCard voterCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Citizen> Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(VoterCard voterCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Mark(Citizen citizen, int keyPhrase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Mark(Citizen citizen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ChangePassword(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InvalidUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InvalidVoterCard(VoterCard voterCard)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
