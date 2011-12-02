8fd87e455bab83005ad51c94ba8cc324fdb490b7
Morten Hyllekilde <hyllekilde88@gmail.com>
2011-12-02 11:40:41 +0100

    changes to DAOMySql

--- DigitalVoterList/DigitalVoterList/Election/DAOMySql.cs
+++ DigitalVoterList/DigitalVoterList/Election/DAOMySql.cs
@@ -269,7 +269,7 @@ namespace DigitalVoterList.Election
                     DoIfNotDbNull(reader, "address", lbl => citizen.Address = reader.GetString(lbl));
                     DoIfNotDbNull(reader, "place_of_birth", lbl => citizen.PlaceOfBirth = reader.GetString(lbl));
                     DoIfNotDbNull(reader, "passport_number", lbl => citizen.PassportNumber = reader.GetInt32(lbl));
-                    //TODO SET HASVOTED
+                    citizen.HasVoted = reader.GetBoolean("has_voted");
                     citizens.Add(citizen);
                 }
 
