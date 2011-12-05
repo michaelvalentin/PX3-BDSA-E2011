7c8d155e672d6654504e2e317d21008dbb4f96b9
Jens Mollerhoj <mollerhoj3@gmail.com>
2011-12-05 15:21:03 +0100

    Added the DataTransformer and RawPerson class

---
+++ DigitalVoterList/DigitalVoterList/Election/DataTransformer.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DataCollector.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DigitalVoterList.Election
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class DataTransformer
+    {
+        public void CollectData()
+        {
+            //Readeer
+
+            //Læs række
+
+            //Generer persons
+
+            //gen quizes
+
+            //
+
+        }
+
+
+    }
+}
---
+++ DigitalVoterList/DigitalVoterList/Election/RawPerson.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RawPerson.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DigitalVoterList.Election
+{
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class RawPerson
+    {
+        public string Name { get; set; }
+    }
+}
