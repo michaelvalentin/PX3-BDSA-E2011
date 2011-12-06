b91750f798c919fd60a7851050f7588d03c8cb02
Morten Hyllekilde <hyllekilde88@gmail.com>
2011-12-06 10:28:52 +0100

    changed cpr to string in DAOMySql

--- DigitalVoterList/DigitalVoterList/Election/Administration/VoterCardPrinter.cs
+++ DigitalVoterList/DigitalVoterList/Election/Administration/VoterCardPrinter.cs
@@ -1,6 +1,7 @@
 ﻿namespace DigitalVoterList.Election.Administration
 {
     using System.Windows;
+    using System.Windows.Controls;
 
     /// <summary>
     /// A printer used to create the physical voter cards
@@ -13,12 +14,12 @@
 
         public void Print(VoterCard voterCard)
         {
-            /*PrintDialog printDlg = new System.Windows.Controls.PrintDialog();
+            PrintDialog printDlg = new System.Windows.Controls.PrintDialog();
             PrintVoterCard newVoterCard = new PrintVoterCard(voterCard);
             if (printDlg.ShowDialog() == true)
             {
                 printDlg.PrintVisual(newVoterCard, "Print Single VoterCard");
-            }*/
+            }
         }
     }
 }
