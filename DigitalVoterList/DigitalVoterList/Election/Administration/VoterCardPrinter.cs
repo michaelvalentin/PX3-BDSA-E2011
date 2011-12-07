0a7d1ae185a4f40a96b38bf7fd17df11ea111a8d
Morten Hyllekilde <hyllekilde88@gmail.com>
2011-12-07 10:25:42 +0100

    added printVoterCard.xaml and .xaml.cs

--- DigitalVoterList/DigitalVoterList/Election/Administration/VoterCardPrinter.cs
+++ DigitalVoterList/DigitalVoterList/Election/Administration/VoterCardPrinter.cs
@@ -20,6 +20,13 @@
         
         public void Print(VoterCard voterCard)
         {
+            DAOMySql dao = new DAOMySql();
+            Citizen citizen = (Citizen)dao.LoadPerson(1);
+            VoterCard vc = dao.LoadVoterCard(1);
+            PrintVoterCard voterCard = new PrintVoterCard(vc);
+            PrintDialog dialog = new PrintDialog();
+            if (dialog.ShowDialog() == true)
+            { dialog.PrintVisual(voterCard.printPage, "Print Voter Card"); }
         }
     }
 }
