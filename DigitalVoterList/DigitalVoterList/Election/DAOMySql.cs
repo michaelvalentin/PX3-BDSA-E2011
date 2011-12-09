034bde14d8487dcea0d09e6d1a75c37ce086461f
Michael Valentin <mv@signifly.com>
2011-12-09 14:02:22 +0100

    Worked on new and imrpoved DAOMySql with transactions and contracts. NOT finished yet..

--- DigitalVoterList/DigitalVoterList/Election/DAOMySql.cs
+++ DigitalVoterList/DigitalVoterList/Election/DAOMySql.cs
@@ -11,7 +11,7 @@ namespace DigitalVoterList.Election
 
     public class DAOMySql : IDataAccessObject
     {
-
+        #region Constructor and factory
         private DAOMySql(string connectionString)
         {
             _connectionString = connectionString;
@@ -21,51 +21,31 @@ namespace DigitalVoterList.Election
         {
             return new DAOPermissionProxy(u, new DAOMySql(connectionString));
         }
+        #endregion
 
         #region Implementation of IDataAccessObject
 
-        //DØD BABY!
-        /*public Person LoadPerson(string cpr)
-        {
-            Person p;
-            DoTransaction(() => { p = PriLoadPerson(cpr); });
-            return p;
-        }*/
-
         public Person LoadPerson(string cpr)
         {
+            Contract.Requires(cpr != null);
+            Person p = null;
             return (Person)LoadWithTransaction(() => PriLoadPerson(cpr));
+            return p;
         }
 
-        /*private Person PriLoadPerson(string cpr)
-        {
-            Contract.Requires(CprExists(cpr, trans));
-            MySqlCommand command = Prepare("SELECT id FROM person WHERE cpr=@cpr");
-            command.Parameters.AddWithValue("@cpr", cpr);
-            int personId = 0;
-            Query(command, (object id) => personId = (int)id);
-            return LoadPerson(personId);
-        }*/
-
-
-        //Other way...
         private Person PriLoadPerson(string cpr)
         {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(cpr != null);
             Contract.Requires(Find(new Person() { Cpr = cpr }).Count == 1);
+            Contract.Requires(_transaction != null);
             Contract.Ensures(Contract.Result<Person>() != null);
             MySqlCommand command = Prepare("SELECT id FROM person WHERE cpr=@cpr");
             command.Parameters.AddWithValue("@cpr", cpr);
-            int id = 0;
-            Query(command, (object o) => { id = (int)o; });
+            int id = (int)ScalarQuery(command);
             return PriLoadPerson(id);
         }
 
-
-        /// <summary>
-        /// What person has this id?
-        /// </summary>
-        /// <param name="id">The database id of the person to load</param>
-        /// <returns>The Person object loaded from the database</returns>
         public Person LoadPerson(int id)
         {
             return (Person)LoadWithTransaction(() => PriLoadPerson(id));
@@ -73,8 +53,9 @@ namespace DigitalVoterList.Election
 
         private Person PriLoadPerson(int id)
         {
-            //Contract.Requires(_transaction != null); //Must be included for all "Pri" methods.. Requirement to even make Contracts!!
-            //Contract.Requires(PersonExistsWithId(id, trans));
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(ExistsWithId("person", id), "Person must exist in the database to be loaded.");
+            Contract.Ensures(Contract.Result<Person>() != null);
             MySqlCommand command = Prepare("SELECT * FROM person WHERE id=@id");
             command.Parameters.AddWithValue("id", id);
             Person p = new Person(id);
@@ -87,25 +68,64 @@ namespace DigitalVoterList.Election
             return p;
         }
 
+        //Does this id exist in this database table?
+        private bool ExistsWithId(string tableName, int id)
+        {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(tableName != null);
+            Contract.Requires(id > 0);
+            MySqlCommand cmd = Prepare("SELECT id FROM @tableName WHERE id=@id");
+            cmd.Parameters.AddWithValue("@tableName", tableName);
+            cmd.Parameters.AddWithValue("@id", id);
+            object found = ScalarQuery(cmd);
+            return found != null;
+        }
+
         /// <summary>
         /// What user has this username?
         /// </summary>
         /// <param name="username">The username to search for</param>
-        /// <returns>A user object</returns>
+        /// <returns>A user object, null if no user could be found</returns>
         public User LoadUser(string username)
         {
-            throw new NotImplementedException();
+            User user = null;
+            DoTransaction(() => { user = PriLoadUser(username); });
+            return user;
+        }
+
+        private User PriLoadUser(string username)
+        {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(username != null, "Input username must not be null!");
+            MySqlCommand cmd = Prepare("SELECT id FROM user WHERE user_name=@username");
+            cmd.Parameters.AddWithValue("@username", username);
+            object maybeId = ScalarQuery(cmd);
+            if (maybeId == null) return null;
+            int id = (int)maybeId;
+            return PriLoadUser(id);
         }
 
         /// <summary>
         /// What authenticated user exists with this username and password?
         /// </summary>
         /// <param name="username">The username to search for</param>
-        /// <param name="password">The password to validate with</param>
+        /// <param name="password">The passwordHash to validate with</param>
         /// <returns>An authenticated user object, null if no user matched the details</returns>
-        public User LoadUser(string username, string password)
+        public User LoadUser(string username, string passwordHash)
         {
-            throw new NotImplementedException();
+            Contract.Requires(username != null, "The input username must not be null!");
+            Contract.Requires(passwordHash != null, "The input password must not be null!");
+            User u = null;
+            DoTransaction(() =>
+                              {
+                                  bool valid = PriValidateUser(username, passwordHash);
+                                  if (valid)
+                                  {
+                                      u = PriLoadUser(username);
+                                  }
+                              });
+            u.FetchPermissions(username, passwordHash);
+            return u;
         }
 
         /// <summary>
@@ -115,7 +135,35 @@ namespace DigitalVoterList.Election
         /// <returns>An unauthenticated user obejct.</returns>
         public User LoadUser(int id)
         {
-            throw new NotImplementedException();
+            Contract.Requires(id > 0, "The input id must be larger than zero.");
+            Contract.Requires(ExistsWithId("user", id));
+            User u = null;
+            DoTransaction(() => { u = PriLoadUser(id); });
+            return u;
+        }
+
+        private User PriLoadUser(int id)
+        {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(ExistsWithId("user", id), "User must exist in the database to be loaded.");
+            Contract.Requires(id > 0, "The input id must be larger than zero.");
+            Contract.Ensures(Contract.Result<User>() != null);
+            MySqlCommand cmd = Prepare("SELECT * FROM user u INNER JOIN person p ON u.person_id=p.id WHERE id=@id");
+            cmd.Parameters.AddWithValue("@id", id);
+            User u = new User(id);
+            Query(cmd, rdr =>
+                           {
+                               rdr.Read();
+                               DoIfNotDbNull(rdr, "name", lbl => { u.Name = rdr.GetString(lbl); });
+                               DoIfNotDbNull(rdr, "address", lbl => { u.Address = rdr.GetString(lbl); });
+                               DoIfNotDbNull(rdr, "cpr", lbl => { u.Cpr = rdr.GetString(lbl); });
+                               DoIfNotDbNull(rdr, "place_of_birth", lbl => { u.PlaceOfBirth = rdr.GetString(lbl); });
+                               DoIfNotDbNull(rdr, "passport_number", lbl => { u.PassportNumber = rdr.GetString(lbl); });
+                               u.Username = rdr.GetString("user_name");
+                               u.Title = rdr.GetString("title");
+                               u.UserSalt = rdr.GetString("user_salt");
+                           });
+            return u;
         }
 
         /// <summary>
@@ -126,7 +174,24 @@ namespace DigitalVoterList.Election
         /// <returns>The id of a validated user. 0 if no user can be found.</returns>
         public bool ValidateUser(string username, string passwordHash)
         {
-            throw new NotImplementedException();
+            Contract.Requires(username != null, "The username must not be null!");
+            Contract.Requires(passwordHash != null, "The password hash must not be null!");
+            object result = LoadWithTransaction(() => PriValidateUser(username, passwordHash));
+            return (bool)result;
+        }
+
+        private bool PriValidateUser(string username, string passwordHash)
+        {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(username != null, "The username must not be null!");
+            Contract.Requires(passwordHash != null, "The password hash must not be null!");
+            MySqlCommand cmd = Prepare("SELECT id FROM user WHERE user_name=@username AND password_hash=@passwordHash");
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@passwordHash", passwordHash);
+            object result = ScalarQuery(cmd);
+            if (result == null) return false;
+            int output = (int)result;
+            return output == 1;
         }
 
         /// <summary>
@@ -136,7 +201,32 @@ namespace DigitalVoterList.Election
         /// <returns>A set of allowed actions</returns>
         public HashSet<SystemAction> GetPermissions(User u)
         {
-            throw new NotImplementedException();
+            Contract.Requires(u != null, "Input user must not be null!");
+            Contract.Ensures(Contract.Result<HashSet<SystemAction>>() != null);
+            var result = new HashSet<SystemAction>();
+            DoTransaction(() => { result = PriGetPermissions(u); });
+            return result;
+        }
+
+        private HashSet<SystemAction> PriGetPermissions(User user)
+        {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(user != null, "The input user must not be null!");
+            Contract.Ensures(Contract.Result<HashSet<SystemAction>>() != null);
+            var output = new HashSet<SystemAction>();
+            if (user.DbId < 1) return output; //The user CAN not exist in the database...
+            MySqlCommand cmd =
+                Prepare("SELECT a.label FROM action a INNER JOIN permission p ON a.id = p.action_id WHERE p.user_id=@id");
+            cmd.Parameters.AddWithValue("@id", user.DbId);
+            Query(cmd, rdr =>
+                           {
+                               while (rdr.Read())
+                               {
+                                   SystemAction action = SystemActions.getSystemAction(rdr.GetString(0));
+                                   output.Add(action);
+                               }
+                           });
+            return output;
         }
 
         /// <summary>
@@ -146,7 +236,35 @@ namespace DigitalVoterList.Election
         /// <returns>The voting venues where the user works</returns>
         public HashSet<VotingVenue> GetWorkplaces(User u)
         {
-            throw new NotImplementedException();
+            Contract.Requires(u != null, "Input user must not be null!");
+            Contract.Ensures(Contract.Result<HashSet<VotingVenue>>() != null);
+            var result = new HashSet<VotingVenue>();
+            DoTransaction(() => { result = PriGetWorkplaces(u); });
+            return result;
+        }
+
+        private HashSet<VotingVenue> PriGetWorkplaces(User user)
+        {
+            Contract.Requires(_transaction != null, "This method must be performed in a transaction.");
+            Contract.Requires(user != null, "The input user must not be null!");
+            Contract.Ensures(Contract.Result<HashSet<VotingVenue>>() != null);
+            var output = new HashSet<VotingVenue>();
+            if (user.DbId < 1) return output; //The user CAN not exist in the database...
+            MySqlCommand cmd =
+                Prepare("SELECT v.id, v.address, v.name FROM user u INNER JOIN workplace w ON u.id = w.user_id INNER JOIN voting_venue v ON v.id = w.voting_venue_id WHERE u.id=@id");
+            cmd.Parameters.AddWithValue("@id", user.DbId);
+            Query(cmd, rdr =>
+            {
+                while (rdr.Read())
+                {
+                    VotingVenue venue = new VotingVenue(
+                        rdr.GetInt32("id"),
+                        rdr.GetString("name"),
+                        rdr.GetString("address"));
+                    output.Add(venue);
+                }
+            });
+            return output;
         }
 
         /// <summary>
@@ -472,7 +590,7 @@ namespace DigitalVoterList.Election
         }
 
         //Brug en scalar
-        private void Query(MySqlCommand cmd, Action<object> func)
+        private void ScalarQuery(MySqlCommand cmd, Action<object> func)
         {
             try
             {
@@ -486,10 +604,10 @@ namespace DigitalVoterList.Election
         }
 
         //Returner scalar
-        private object Query(MySqlCommand cmd, Func<object, object> func)
+        private object ScalarQuery(MySqlCommand cmd)
         {
             object o = null;
-            Query(cmd, (object obj) => { o = func(obj); });
+            ScalarQuery(cmd, obj => o = obj);
             return o;
         }
 
@@ -509,6 +627,16 @@ namespace DigitalVoterList.Election
             finally
             {
                 if (rdr != null) rdr.Close();
+            }
+        }
+
+        //Do this only if the supplied lable on the supplied reader is not db-null
+        private void DoIfNotDbNull(MySqlDataReader rdr, string label, Action<string> act)
+        {
+            int ordinal = rdr.GetOrdinal(label);
+            if (!rdr.IsDBNull(ordinal))
+            {
+                act(label);
             }
         }
         #endregion
